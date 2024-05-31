af3465e46164649956dca7b9d6c0615c038e6b98
elsrtalex <166781330+elsrtalex@users.noreply.github.com>
2024-05-31 00:08:51 -0500

    Update Blade Ball.lua

--- Blade Ball.lua
+++ Blade Ball.lua
@@ -402,13 +402,13 @@ end)
 
 
 -- Definir el nivel de precisión (0.0 a 1.0)
-local parry_accuracy = 1.0 -- 90% de precisión
+local parry_accuracy = 0.9 -- 90% de precisión
 
 -- Definir el tiempo de spam (en segundos)
-local spam_time = 1.90 -- Puede ajustar este valor entre 0.0 y 10.0
+local spam_time = 0.50 -- Puede ajustar este valor entre 0.0 y 10.0
 
 -- Definir el tiempo mínimo entre parrys (en segundos)
-local hit_time = 0.50 -- Puede ajustar este valor entre 0.0 y 1.0
+local hit_time = 0.75 -- Puede ajustar este valor entre 0.0 y 1.0
 
 -- Mejorar la función de incremento y decremento de hit_Count
 local function increment_hit_count()
