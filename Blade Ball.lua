c9a8ad392ae01f39dd414c48a1588456f2513928
elsrtalex <166781330+elsrtalex@users.noreply.github.com>
2024-06-03 20:01:58 -0500

    Update Blade Ball.lua

--- Blade Ball.lua
+++ Blade Ball.lua
@@ -166,7 +166,7 @@ game.StarterGui:SetCore("SendNotification", {
     ReplicatedStorage.Remotes.ParrySuccessAll.OnClientEvent:Connect(function()
         aura_table.hit_Count += 1
     
-        task.delay(0.005, function()
+        task.delay(0.20, function()
             aura_table.hit_Count -= 1
         end)
     end)
