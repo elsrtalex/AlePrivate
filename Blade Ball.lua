fec472f541ea2c13d876ec9e058976638e2bd6cb
elsrtalex <166781330+elsrtalex@users.noreply.github.com>
2024-06-03 23:49:57 -0500

    Update Blade Ball.lua

--- Blade Ball.lua
+++ Blade Ball.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/elsrtalex/AlePrivate/main/obf_9uLSS40sf23GE1WbeH2s4V3EPX4dvBFh2TiC87i3Utm2P29I981qNUh0iWPVfnkZ.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/elsrtalex/AlePrivate/main/MainLoader.lua'))()
